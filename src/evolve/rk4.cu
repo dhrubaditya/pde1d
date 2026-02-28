1743892a42c14acadf1c7a98bce8f6f47fad2d83
dhrubaditya <DhrubadityaMitra@proton.me>
2026-02-28 10:27:42 +0100

    RK2 working(?) writing RK4 again

--- src/evolve/rk4.cu
+++ src/evolve/rk4.cu
@@ -88,16 +88,52 @@ void TimeStep_free_device_memory(TimeStepDeviceData& dev)
 void ExpRK4(cufftDoubleComplex* d_psi, int N,  double dt, 
 		       TimeStepDeviceData& dev)
 {
-    if (N <= 0) return;
     const int threads = BLOCK_SIZE;
     const int blocks  = (N + threads - 1) / threads;
-    double tt = 0;
+     tt = 0
     // First transform variable
+    // Y = exp(-G*tt)\psi
     exp_transform(dev.d_Y, d_psi, tt, false, N);
-    // Stage 1: k1 = dt * f(Y) -> dev.d_k1
+    // 1st evaluation of RHS: k1 = f(Y) -> dev.d_k1
+    // d_k1 = exp(-G*tt)*NLIN(psi)
     compute_rhs(dev.d_k1, d_psi, tt, N, 1);
     CUDA_CHECK(cudaGetLastError());
     CUDA_CHECK(cudaDeviceSynchronize());
+    // take 1st step:
+    //Ytemp = Y + 0.5*dt*k1 ; tt = tt + dt/2;
+    // psi(tt+dt/2) = Ytemp*exp(G* (tt+dt/2) )
+    combine_stage_kernel<<<blocks, threads>>>(dev.d_Ytemp, dev.d_Y,
+					      dev.d_k1, 0.5*dt, N );
+    CUDA_CHECK(cudaDeviceSynchronize());
+    tt = dt/2.;
+    exp_transform(d_psi, dev.d_Ytemp, -tt, false, N); //actually inverse transform
+    // 2nd evaluation of RHS: k2 = f(Ytemp) -> dev.d_k2
+    // d_k2 = exp(-G*tt)*NLIN(psi)
+    compute_rhs(dev.d_k2, d_psi, tt, N, 2);
+    //take 2nd step:
+    //Ytemp = Y + 0.5*dt*k2 ; tt = dt/2;
+    // psi(dt/2) = Ytemp*exp(G* (dt/2) )
+    combine_stage_kernel<<<blocks, threads>>>(dev.d_Ytemp, dev.d_Y,
+					      dev.d_k2, 0.5*dt, N );
+    CUDA_CHECK(cudaDeviceSynchronize());
+    tt = dt/2.;
+    exp_transform(d_psi, dev.d_Ytemp, -tt, false, N); //actually inverse transform
+    // 3rd evaluation of RHS: k3 = f(Ytemp) -> dev.d_k3
+    // d_k3 = exp(-G*tt)*NLIN(psi)
+    tt = dt/2
+    compute_rhs(dev.d_k2, d_psi, tt, N, 3);
+    //take 3rd step:
+    //Ytemp = Y + dt*k3 ; tt = dt;
+    // psi(dt) = Ytemp*exp(G* dt )
+    tt = dt;
+    combine_stage_kernel<<<blocks, threads>>>(dev.d_Ytemp, dev.d_Y,
+					      dev.d_k3, dt, N );
+    CUDA_CHECK(cudaDeviceSynchronize());
+    tt = dt;
+    exp_transform(d_psi, dev.d_Ytemp, -tt, false, N); //actually inverse transform
+
+
+
     
     // Stage 2: Ytemp = Y + 0.5*k1 ; k2 = dt * f(Ytemp)
     combine_stage_kernel<<<blocks, threads>>>(dev.d_Ytemp,
@@ -141,7 +177,35 @@ void ExpRK4(cufftDoubleComplex* d_psi, int N,  double dt,
     CUDA_CHECK(cudaDeviceSynchronize());
  }
 //--------------------------------------------------//
-//-----------------------------------//
+void ExpRK2(cufftDoubleComplex* d_psi, int N,  double dt, 
+		       TimeStepDeviceData& dev)
+{
+    const int threads = BLOCK_SIZE;
+    const int blocks  = (N + threads - 1) / threads;
+    double tt = 0;
+    // First transform variable
+    // Y = exp(-G*tt)\psi
+    exp_transform(dev.d_Y, d_psi, tt, false, N);
+    // 1st evaluation of RHS: k1 = f(Y) -> dev.d_k1
+    // d_k1 = exp(-G*tt)*NLIN(psi)
+    compute_rhs(dev.d_k1, d_psi, tt, N, 1);
+    // take half step:
+    //Ytemp = Y + 0.5*dt*k1 ; tt = tt + dt/2;
+    // psi(tt+dt/2) = Ytemp*exp(G* (tt+dt/2) )
+    combine_stage_kernel<<<blocks, threads>>>(dev.d_Ytemp, dev.d_Y,
+					      dev.d_k1, 0.5*dt, N );
+    CUDA_CHECK(cudaDeviceSynchronize());
+    tt = tt + dt/2.;
+    exp_transform(d_psi, dev.d_Ytemp, -tt, false, N); //actually inverse transform
+    // 2nd evaluation of RHS: k2 = f(Ytemp) -> dev.d_k2
+    // d_k2 = exp(-G*tt)*NLIN(psi)
+    compute_rhs(dev.d_k2, d_psi, tt, N, 2);
+    // Final update Y <- Y + d_k2*dt
+    euler_update_kernel<<<blocks, threads>>>(dev.d_Y, dev.d_k2, dt, N);
+    exp_transform(d_psi, dev.d_Y,-dt, false, N); //actually inverse transform
+    CUDA_CHECK(cudaDeviceSynchronize());
+ }
+//---------------------------
 void ExpEuler(cufftDoubleComplex* d_psi, int N,  double dt, 
 		       TimeStepDeviceData& dev)
 {
@@ -151,19 +215,21 @@ void ExpEuler(cufftDoubleComplex* d_psi, int N,  double dt,
     // First transform variable
     // Y = exp(-G*tt)\psi
     exp_transform(dev.d_Y, d_psi, tt, false, N);
+    // step one
     // d_k1 = exp(-G*tt)*NLIN(psi)
     compute_rhs(dev.d_k1, d_psi, tt, N, 1);
     CUDA_CHECK(cudaGetLastError());
     CUDA_CHECK(cudaDeviceSynchronize());     
     // Final update Y <- Y + d_k1*dt
     euler_update_kernel<<<blocks, threads>>>(dev.d_Y, dev.d_k1, dt, N);
-    exp_transform(d_psi, dev.d_Y,-(tt+dt), false, N); //actually inverse transform
+    exp_transform(d_psi, dev.d_Y,-dt, false, N); //actually inverse transform
     CUDA_CHECK(cudaGetLastError());
     CUDA_CHECK(cudaDeviceSynchronize());
  }
 //------------------------------------------//
 void ExpScheme(cufftDoubleComplex* d_psi, int N,  double dt, 
 	       TimeStepDeviceData& dev){
-  //ExpRK4(d_psi, N, dt, dev);
-  ExpEuler(d_psi, N, dt, dev);
+   //ExpEuler(d_psi, N, dt, dev);
+   //ExpRK2(d_psi, N, dt, dev);
+   ExpRK4(d_psi, N, dt, dev);
 }
